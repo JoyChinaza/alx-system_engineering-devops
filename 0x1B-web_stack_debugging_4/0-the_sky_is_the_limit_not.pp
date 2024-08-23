17e3471e3e6e102f0df8a02a9ee2c0e4068717fd
your username <nwosuchinaza22@gmail.com>
2024-08-22 21:34:45 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,12 @@
-# Testing how well our web server setup featuring Nginx is doing under pressure
+# Fix problem of high amount of requests
 
-# increaing the amount of requests
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-
-#restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
